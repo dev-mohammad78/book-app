947fe365ea92b908ac0faea35c580daa06e2ee18
dev-mohammad78 <dev.mohammad78@gmail.com>
2026-07-06 00:03:56 +0330

    LocalDatabase (mockData) added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { books } from "../src/constant/mockData";
 function App() {
   return (
     <>
       <h1 className="text-2xl">Book app</h1>
+      {books.map((book) => (
+        <p key={book.id}>{book.title}</p>
+      ))}
     </>
   );
 }
